80701f4662e9a23366deb0ef69d410cbe30b29bd
Russell Wyatt <rswyatt1@gmail.com>
2013-05-28 17:09:38 -0700

    Initial Commit

---
+++ SplashTracks/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SplashTracks
+//
+//  Created by Russell Wyatt on 5/28/13.
+//  Copyright (c) 2013 Russell Wyatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SplashTracks/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SplashTracks
+//
+//  Created by Russell Wyatt on 5/28/13.
+//  Copyright (c) 2013 Russell Wyatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
